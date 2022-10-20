19febc800e50bf482a20383fb1195d8cf53d0c43
nashvent <anibalventura123@gmail.com>
2022-10-20 15:18:22 -0500

    Add texture with transform

--- textures/main.cpp
+++ textures/main.cpp
@@ -1,12 +1,17 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include "shader_s.h"
 
 #include <iostream>
 #include <experimental/filesystem>
+#include <vector>
+#include "matrix.cpp"
+
+
 namespace fs = std::experimental::filesystem;
 
 
@@ -16,6 +21,22 @@ void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+
+std::vector<float> vertices = {
+    0.0f, 0.2f, 0.0f, // A 
+    0.1f, 0.1f, 0.0f, // B
+    0.05f, -0.05f, 0.0f, // C
+    0.0f, 0.037f, 0.0f, // D
+    -0.05f, -0.05f, 0.0f, // E
+    -0.1f, 0.1f, 0.0f, // F
+};
+
+std::vector<unsigned int> indices = { 
+     0, 2, 3,
+            0, 4, 3,   
+            5, 3, 1,
+};
 
 int main()
 {
@@ -54,19 +75,6 @@ int main()
     // ------------------------------------
     Shader ourShader("4.1.texture.vs", "4.1.texture.fs"); 
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
-    };
-    unsigned int indices[] = {  
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -75,19 +83,19 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW);
+    
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), indices.data(), GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     // texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
 
@@ -138,7 +146,11 @@ int main()
         // render container
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW);
+        
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -162,8 +174,35 @@ int main()
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
+    MatrixTransform *curr_matrix = nullptr;
+    
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
+        std::vector<float>tVector = {-0.01f, 0.0, 0.0};
+        curr_matrix = new MatrixTransform(tVector, 't');
+
+    }
+
+    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
+        std::vector<float>tVector = {0.01f, 0.0, 0.0};
+        curr_matrix = new MatrixTransform(tVector, 't');
+    }
+
+    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
+        std::vector<float>tVector = {0.0, 0.01f, 0.0};
+        curr_matrix = new MatrixTransform(tVector, 't');
+    }
+
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
+        std::vector<float>tVector = {0.0, -0.01f, 0.0};
+        curr_matrix = new MatrixTransform(tVector, 't');
+    }
+
+    if(curr_matrix != nullptr){  
+      vertices = curr_matrix->multiply(vertices);
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
